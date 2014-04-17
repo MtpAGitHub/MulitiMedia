a20460a15f3b2dbb55c935750bfecf0e12792915
Mark Addison <m.addison@sussex.ac.uk>
2014-04-17 22:37:50 +0100

    Image flip pictures on the fly

--- public_html/js/startgame.js
+++ public_html/js/startgame.js
@@ -11,9 +11,27 @@ function gameStart() {
     );
 }
 
-function selectItem() {
-    document.querySelector(".flip-container").classList.toggle("flip");
-    startDebugMsg("You pressed","flip");
+function selectItem(pressedButton) {
+    var queryElement;
+    var flipImage = document.createElement("img");
+    var flipElement;
+    
+    switch (pressedButton) {
+        case "1":
+            queryElement = "#one";
+            flipImage.src = "../icons/music.png";
+            flipElement = document.getElementById("back1");
+            flipElement.appendChild(flipImage);
+            break;
+        case "2":
+            queryElement = "#two";
+            break;
+        default:
+            queryElement = "none";
+            break;
+    }
+    document.querySelector(queryElement).classList.toggle("flip");
+    startDebugMsg(queryElement,pressedButton);
     return false;
 }
 
