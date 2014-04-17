f4a55b289f559230689a1027ae0a0aabc2a7c5ce
Mark Addison <m.addison@sussex.ac.uk>
2014-04-17 10:21:02 +0100

    second page layout part II

--- public_html/js/startgame.js
+++ public_html/js/startgame.js
@@ -1,7 +1,16 @@
-function gameStart() {
+function gameStart(questionPaper) {
+    var questionText = questionPaper.text(0, 20, "Some starting question");
+    questionText.attr(
+            {
+                'font-family': "arial",
+                'font-size': 24,
+                'text-anchor': "start",
+                'fill': "white"
+            }
+    );
     startDebugMsg("You pressed","START");
 }
 
 function startDebugMsg(a,b) {
-	document.getElementById("mainPage").innerHTML="This is the main page " + a + ":" + b;
+	document.getElementById("debug").innerHTML="Game page. " + a + ":" + b;
 }
